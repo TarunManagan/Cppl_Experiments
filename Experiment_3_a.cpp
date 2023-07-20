04c1b46b32ddd3eaa4e7ed33d9139c576cfe923d
TarunManagan <139469256+TarunManagan@users.noreply.github.com>
2023-07-21 00:41:50 +0530

    Add files via upload

---
+++ Experiment_3_a.cpp
@@ -0,0 +1,22 @@
+/*  PRN:22070123118
+    Experiment no:3(a)
+*/
+#include<iostream>
+using namespace std;
+
+int main(){
+   string PRN = "23118";
+   for (int i=0;i!='\0'; i++){
+    cout<<PRN[i]<<endl;
+   }
+
+return 0;
+}
+
+/*  OUTPUT
+8
+1
+1
+3
+2
+*/
