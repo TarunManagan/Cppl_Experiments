df02aca1198389d5af8558e15f37d423b211a35e
TarunManagan <139469256+TarunManagan@users.noreply.github.com>
2023-07-21 11:04:59 +0530

    Update Experiment_3_a.cpp

--- Experiment_3_a.cpp
+++ Experiment_3_a.cpp
@@ -5,18 +5,19 @@
 using namespace std;
 
 int main(){
-   string PRN = "23118";
-   for (int i=0;i!='\0'; i++){
-    cout<<PRN[i]<<endl;
+    
+   char PRN[] = "23118";
+   for (int i=0; i!='\0'; i++){
+       cout<<PRN[i]<<endl;
    }
 
 return 0;
 }
 
 /*  OUTPUT
-8
-1
-1
-3
 2
+3
+1
+1
+8
 */
