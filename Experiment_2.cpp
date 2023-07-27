1356b98c334a00369ea7a06e86e4ffdef00da13b
TarunManagan <139469256+TarunManagan@users.noreply.github.com>
2023-07-28 00:57:09 +0530

    Update Experiment_2.cpp

--- Experiment_2.cpp
+++ Experiment_2.cpp
@@ -5,11 +5,11 @@
 using namespace std;
 
 int main() {
-   int Integer_var;
-   float Float_var;
-   double Double_var;
-   char Char_var;
-   bool flag;
+   int Integer_var; //Integer takes whole numbers --> 1,2,4, etc. --> 4 bytes
+   float Float_var; //Float takes fractional numbers upto 7 decimal places --> 1.903, 0.9243 etc. --> 4 bytes
+   double Double_var; //Double takes fractional numbers upto 15 decimal places --> 1.9037849, 0.92438935364 etc. --> 8 bytes
+   char Char_var; //Character takes single characters enclosed in single quotes --> 'a', 'H', '$' etc. --> 1 byte
+   bool flag; //Boolean takes either true (1) or false (0) --> 1 byte
    
    cout<< "Enter any integer value: ";
    cin >> Integer_var;
@@ -22,11 +22,11 @@ int main() {
    cout<< "Enter any boolean value: ";
    cin >> flag;
    
-   cout<<"Value of Integer_var is: "<< Integer_var<<endl;
-   cout<<"Value of Float_var is: "<< Float_var<<endl;
-   cout<<"Value of Double_var is: "<< Double_var<<endl;
-   cout<<"Value of Char_var is: "<< Char_var<<endl;
-   cout<<"Value of flag is: "<< flag<<endl;
+   cout<<"Value of Integer_var is: "<< Integer_var<<endl; //Printing the value Integer_var
+   cout<<"Value of Float_var is: "<< Float_var<<endl; //Printing the value Float_var
+   cout<<"Value of Double_var is: "<< Double_var<<endl; //Printing the value Double_var
+   cout<<"Value of Char_var is: "<< Char_var<<endl; //Printing the value Char_var
+   cout<<"Value of flag is: "<< flag<<endl; //Printing the value flag
 return 0;
 }
 /*  OUTPUT
@@ -55,7 +55,7 @@ int main() {
    char Char_var;
    bool flag;
    
-   cout<<"The size of Integer_var is: "<< sizeof(Integer_var)<<endl;
+   cout<<"The size of Integer_var is: "<< sizeof(Integer_var)<<endl; //sizeof operator is used to find the size of the memory allocated to that particular data type
    cout<<"The size of Float_var is: "<< sizeof(Float_var)<<endl;
    cout<<"The size of Double_var is: "<< sizeof(Double_var)<<endl;
    cout<<"The size of Char_var is: "<< sizeof(Char_var)<<endl;
@@ -82,7 +82,7 @@ int main() {
    double Double_var;
    char Char_var;
    bool flag;
-   const float pi = 3.14;
+   const float pi = 3.14; // Constant is used to fix the value of any variable --> Cannot be changed by the user
    
    cout<< "Enter any integer value: ";
    cin >> Integer_var;
@@ -125,7 +125,7 @@ using namespace std;
 
 int main() {
    bool imagecolor = true;
-   if(imagecolor){
+   if(imagecolor){ //Checks whether imagecolor is true or not, and if true it prints the below statement or else, prints the statement in the else part.
     cout<<"Image is colorfull";
    }else{
     cout<<"Image is not colorfull";
@@ -146,7 +146,7 @@ int main(){
     int a,b;
     cout<<"Enter two numbers: ";
     cin>> a >> b;
-    if(a>b){
+    if(a>b){ //Checks whether a is greater than b or not, and if true it prints the below statement or else, prints the statement in the else part. --> GREATER THAN OPERATOR IS USED
         cout<<"a is greater than b";
     }else{
         cout<<"b is greater than a ";
