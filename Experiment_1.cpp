cc8a03979385a4f59455802b5336f151501448e9
TarunManagan <139469256+TarunManagan@users.noreply.github.com>
2023-07-28 00:18:58 +0530

    Update Experiment_1.cpp

---
+++ Experiment_1.cpp
@@ -0,0 +1,40 @@
+/*  PRN:22070123118
+    Experiment no:1(a)
+*/
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Hello World!";
+    return 0;
+}
+/*  OUTPUT
+Hello World!
+*/
+
+/*  PRN:22070123118
+    Experiment no:1(b)
+*/
+#include<iostream>
+using namespace std;
+
+int main(){
+    float a,b;
+    cout<<"Enter two numbers: ";
+    cin>> a >> b;
+    cout << "The sum of a and b is: " << a+b <<endl;
+    cout << "The difference of a and b is: " << a-b<<endl;
+    cout << "The product of a and b is: " << a*b<<endl;
+    cout << "The division of a and b is: " << a/b<<endl;
+    return 0;
+}
+/*  OUTPUT
+Enter two numbers: 3 2
+The sum of a and b is: 5
+The difference of a and b is: 1
+The product of a and b is: 6
+The division of a and b is: 1.5
+*/
+
+
+
