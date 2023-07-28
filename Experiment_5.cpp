debd67b80f36005b8286ec912257b6c4da71a733
TarunManagan <139469256+TarunManagan@users.noreply.github.com>
2023-07-28 12:11:52 +0530

    Update Experiment_5.cpp

--- Experiment_5.cpp
+++ Experiment_5.cpp
@@ -170,6 +170,26 @@ Enter the value of n: 1
 */
 
 /*PRN: 22070123118
+Experiment no: 4(d)*/
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n=1;
+    do{
+        cout<<n<<"\t";
+        n++;
+    }while(n<=50);
+   
+       return 0;
+}
+
+/*OUTPUT
+1	2	3	4	5	6	7	8	9	10	11	12	13	14	15	16	17	18	19	20	21	22	23	24	25
+26	27	28	29	30	31	32	33	34	35	36	37	38	39	40	41	42	43	44	45	46	47	48	49	50
+*/
+
+/*PRN: 22070123118
 Experiment no: 4(e)*/
 #include <iostream>
 using namespace std;
