9c08846fed600b509de0e2b931df09c296695fb3
TarunManagan <139469256+TarunManagan@users.noreply.github.com>
2023-08-04 11:21:06 +0530

    Update Experiment_7.cpp

--- Experiment_7.cpp
+++ Experiment_7.cpp
@@ -23,7 +23,7 @@ using namespace std;
 
 int main() {
     int n;
-    cout<<"Eter the size of array: ";
+    cout<<"Enter the size of array: ";
     cin>>n;
     int a1[n];
     for(int i=0; i<n; i++){
@@ -103,4 +103,39 @@ Enter the value to find in the array: 45
 
 Enter the value to find in the array: 67
 The number is not present in the array
+*/
+
+/*PRN: 22070123118
+Experiment-no: 7(e)*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n,sum=0;
+    float avg;
+    cout<<"Enter the size of array: ";
+    cin>>n;
+    int a1[n];
+    for(int i=0; i<n; i++){
+        cout<<"Enter the elements of the array: ";
+        cin>>a1[i];
+    }
+    for(int i=0; i<n; i++){
+        sum=sum+a1[i];
+        avg=sum/n;
+    }
+    cout<<"The sum of elements is: "<<sum<<endl;
+    cout<<"The average of elements is: "<<avg;
+return 0;
+}
+/*OUTPUT
+Enter the size of array: 5
+Enter the elements of the array: 1
+Enter the elements of the array: 2
+Enter the elements of the array: 3
+Enter the elements of the array: 4
+Enter the elements of the array: 5
+The sum of elements is: 15
+The average of elements is: 3	
 */
