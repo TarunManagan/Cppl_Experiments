56c66b4c9e26af913df7ba12db4281613c711a80
TarunManagan <139469256+TarunManagan@users.noreply.github.com>
2023-08-09 10:32:52 +0530

    Update Experiment_8.cpp

--- Experiment_8.cpp
+++ Experiment_8.cpp
@@ -93,3 +93,43 @@ Enter second string: World!
 Concatenated string is: Hello World!
 Lenght of the concatenated string is: 12
 */
+
+/*PRN: 22070123118
+Experiment-no: 8(d)*/
+#include <iostream>
+#include<string.h>
+using namespace std;
+
+int main() {
+    string str1,str2;
+    int n,count=0;
+    cout<<"Enter a string: ";
+    getline(cin,str1);
+    for(int i=0; str1[i]!='\0'; i++){
+        n++;
+    }
+    cout<<"The original string is: "<<str1<<endl;
+    cout<<"The reverse string is: ";
+    for(int i=n; i>=0; i--){
+        str2[i] = str1[i];
+        cout<<str2[i];
+    }
+    for(int i=0; str1[i]!='\0' && str2[i]!='\0'; i++){
+        if(str1[i]==str2[i]){
+            count++; 
+        }
+    }
+    if(count==n){
+        cout<<endl<<"It is a Palindrome";
+    }else{
+        cout<<endl<<"It is not a Palindrome";
+    }
+    
+return 0;
+}
+
+/*OUTPUT
+Enter a string: malayalam
+The original string is: malayalam
+The reverse string is: 
+*/
