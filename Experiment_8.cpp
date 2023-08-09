b20c4361b25ed81ee0dba6ec7cbb7a3921ac6f67
TarunManagan <139469256+TarunManagan@users.noreply.github.com>
2023-08-09 10:42:20 +0530

    Update Experiment_8.cpp

--- Experiment_8.cpp
+++ Experiment_8.cpp
@@ -95,7 +95,7 @@ Lenght of the concatenated string is: 12
 */
 
 /*PRN: 22070123118
-Experiment-no: 8(d)*/
+Experiment-no: 8(e)*/
 #include <iostream>
 #include<string.h>
 using namespace std;
@@ -131,5 +131,32 @@ return 0;
 /*OUTPUT
 Enter a string: malayalam
 The original string is: malayalam
-The reverse string is: 
+The reverse string is: malayalam
+It is a Palindrome
+*/
+
+/*PRN: 22070123118
+Experiment-no: 8(f)*/
+#include <iostream>
+#include<string.h>
+using namespace std;
+
+int main() {
+    string str1;
+    cout<<"Enter a string: ";
+    getline(cin,str1);
+    cout<<"The original string is: "<<str1<<endl;
+    cout<<"The new string is: ";
+    for(int i=0; str1[i]!='\0'; i++){
+        cout<<char(str1[i]-32);
+    
+    }
+    
+return 0;
+}
+
+/*OUTPUT
+Enter a string: tarun
+The original string is: tarun
+The new string is: TARUN
 */
