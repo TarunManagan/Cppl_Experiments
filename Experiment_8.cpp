5daba952993baa7b554f3c9c8d137e30c923e39f
TarunManagan <139469256+TarunManagan@users.noreply.github.com>
2023-08-09 09:38:42 +0530

    Update Experiment_8.cpp

--- Experiment_8.cpp
+++ Experiment_8.cpp
@@ -25,11 +25,11 @@ Experiment-no: 8(b)*/
 using namespace std;
 
 int main() {
-    string name;
+    string str1;
     int count =0;
-    cout<<"Enter your name: ";
-    getline(cin,name);
-    for(int i=0; name[i]!='\0';i++){
+    cout<<"Enter a string: ";
+    getline(cin,str1);
+    for(int i=0; str1[i]!='\0';i++){
         count++;
     }
     cout<<"The lenght of the string is: "<<count;
@@ -38,7 +38,7 @@ return 0;
 }
 
 /*OUTPUT
-Enter your name: Hi My Name is Tarun
+Enter a string: Hi My Name is Tarun
 The lenght of the string is: 19
 */
 
