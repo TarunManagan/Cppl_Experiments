48138821691c62db5ce9bc9d2b69512bf6f715be
TarunManagan <139469256+TarunManagan@users.noreply.github.com>
2023-08-16 10:04:25 +0530

    Update Experiment_10.cpp

--- Experiment_10.cpp
+++ Experiment_10.cpp
@@ -3,21 +3,16 @@ Experiment No: 10(a)*/
 #include <iostream>
 using namespace std;
 int main(){
-   int a,*aptr;
-   cout<<"Enter the value of a: ";
-   cin>>a;
+   int a=10,*aptr;
    aptr=&a;
-   cout<<"The original value of a is: "<<a<<endl;
+   cout<<"The original value of a is: "<<*aptr<<endl;
    cout<<"The address of a is: "<<aptr<<endl;
-   cout<<"Enter the new value of a: ";
-   cin>>*aptr;
-   cout<<"The new value of a is: "<<a;
+   *aptr=15;
+   cout<<"The new value of a is: "<<*aptr;
     return 0;
 }
 /*OUPUT
-Enter the value of a: 10
 The original value of a is: 10
-The address of a is: 0x7ffe6153f454
-Enter the new value of a: 15
+The address of a is: 0x7fff6d64eef4
 The new value of a is: 15
 */
