811030194d8330f282ed21cd6ea25c2c64e7e3bf
TarunManagan <139469256+TarunManagan@users.noreply.github.com>
2023-08-16 10:30:39 +0530

    Update Experiment_10.cpp

--- Experiment_10.cpp
+++ Experiment_10.cpp
@@ -16,3 +16,26 @@ The original value of a is: 10
 The address of a is: 0x7fff6d64eef4
 The new value of a is: 15
 */
+
+*PRN: 22070123118
+Experiment No: 10()*/
+#include <iostream>
+using namespace std;
+int main(){
+   int a=10,*aptr;
+   aptr= &a;
+   cout<<"The original value of a is: "<<*aptr<<endl;
+   cout<<"The original address of a is: "<<aptr<<endl;
+   *aptr=*aptr+1;
+   aptr++;
+   &aptr= *aptr;
+   cout<<"The incremented value of a is: "<<*aptr<<endl;
+   cout<<"The new address of a is: "<<aptr<<endl;
+    return 0;
+}
+/*OUPUT
+The original value of a is: 10
+The original address of a is: 0x7ffee6ade564
+The incremented value of a is: 11
+The new address of a is: 0x7ffee6ade564
+*/
