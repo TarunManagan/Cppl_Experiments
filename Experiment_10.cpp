2c56eefe318199682398eb9593cbe16f10a8e2ef
TarunManagan <139469256+TarunManagan@users.noreply.github.com>
2023-08-16 09:55:12 +0530

    Update Experiment_10.cpp

--- Experiment_10.cpp
+++ Experiment_10.cpp
@@ -16,3 +16,26 @@ Enter the value of a: 10
 The value of aptr is: 10
 The address of aptr is: 0x7fff7edb8364
 */
+
+/*PRN: 22070123118
+Experiment No: 10(b)*/
+#include <iostream>
+using namespace std;
+int main(){
+   int a;
+   int *aptr;
+   cout<<"Enter the value of a: ";
+   cin>>a;
+   cout<<"The original value of a is: "<<a<<endl;
+   aptr=&a;
+   cout<<"Enter the new value of a: ";
+   cin>>*aptr;
+   cout<<"The new value of a is: "<<a;
+    return 0;
+}
+/*OUPUT
+Enter the value of a: 10
+The original value of a is: 10
+Enter the new value of a: 15
+The new value of a is: 15
+*/
