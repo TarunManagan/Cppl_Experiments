d511e3e48b52ad35ed4f48971af825f5e151313e
TarunManagan <139469256+TarunManagan@users.noreply.github.com>
2023-08-18 11:29:46 +0530

    Update Experiment_10.cpp

--- Experiment_10.cpp
+++ Experiment_10.cpp
@@ -34,6 +34,26 @@ int main(){
     return 0;
 }
 /*OUPUT
+
+/*PRN: 22070123118
+Experiment No: 10(c)*/
+#include <iostream>
+using namespace std;
+
+void increment(int *a){
+    (*a)++;
+ }
+int main(){
+   int a=10;
+   cout<<"The original value of a: "<<a<<endl;
+   increment(&a);
+   cout<<"The incremented value of a is: "<<a<<endl;
+   return 0;
+}
+/*OUPUT
+The original value of a: 10
+The incremented value of a is: 11
+*/
 The original value of a is: 10
 The original address of a is: 0x7ffee6ade564
 The incremented value of a is: 11
