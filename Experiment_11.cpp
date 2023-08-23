037b89cf299f1e0cbf36ab860354dd1f8f5bfe1a
TarunManagan <139469256+TarunManagan@users.noreply.github.com>
2023-08-23 09:56:46 +0530

    Update Experiment_11.cpp

--- Experiment_11.cpp
+++ Experiment_11.cpp
@@ -52,3 +52,36 @@ int main(){
 Enter the height, breadth anf lenght of the cube: 4 5 6
 The volume of the cube is: 120
 */
+
+/*PRN: 22070123118
+Experiment_11(c)
+*/
+
+#include<iostream>
+using namespace std;
+class cube{
+        private:
+        double ht,breadth,len;
+        public:
+        double vol_cube(){
+            cout<<"Enter the height, breadth anf lenght of the cube: ";
+            cin>>ht>>breadth>>len;
+            double volume1 = ht*breadth*len;
+            return volume1;
+        }
+        void print(int vol){
+             cout<<endl<<"The volume of the cube is: "<<vol;
+        }
+};
+int main(){
+    double x;
+    cube volume;
+    x=volume.vol_cube();
+    volume.print(x);
+    return 0;
+}
+
+/*OUTPUT
+Enter the height, breadth anf lenght of the cube: 10 6 4
+The volume of the cube is: 240
+*/
