54f3d56f1ab8d1d54a7dced3931bc13a626cefca
TarunManagan <139469256+TarunManagan@users.noreply.github.com>
2023-09-27 10:01:33 +0530

    Update Inheritance.cpp

--- Inheritance.cpp
+++ Inheritance.cpp
@@ -23,3 +23,35 @@ int main(){
 /*OUTPUT
 White
 BMW BMW N52*/
+
+/*Experiment 2*/
+
+#include<iostream>
+using namespace std;
+
+class vehicle{
+    public:
+    string brand = "BMW";
+    string color = "White";
+};
+
+class Features{
+    public:
+    string engine = "BMW N52";
+    string milage = "17.42 Kmpl";
+};
+
+class display:public Features, public vehicle{
+    public:
+    void Display(){
+        cout<< brand<<" "<<color<<" "<<milage;
+    }
+};
+
+int main(){
+    display car;
+    car.Display();
+}
+
+/*OUTPUT
+BMW White 17.42 Kmpl*/
